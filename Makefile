88ddd95f740ec30d28de9970893d5779d2deb17a
student <ishichkok@icloud.com>
2023-12-15 14:58:12 +0300

    alpha 1

---
+++ Makefile
@@ -0,0 +1,25 @@
+install:
+	npm ci
+
+publish:
+	npm publish --dry-run
+
+gendiff:
+	node bin/gendiff.js -h
+
+g:
+	git add .
+	git commit -m "$m"
+	git push
+
+lint:
+	npx eslint .
+
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
+
+test-coverage:
+	npx jest --coverage
+
+fix:
+	npx eslint --fix .
---
+++ bin/gendiff.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+// import gendiff from '../src/index.js';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>') 
+  .action((filePath1, filePath2) => {
+    console.log(gendiff(filePath1, filePath2)); //program.opts().format
+  });
+
+// program.command('split')
+//   .argument('<string>', 'string to split')
+//   .option('--first', 'display just the first substring')
+//   .option('-s, --separator <char>', 'separator character', ',')
+//   .action('privet');
+
+program.parse(process.argv);
